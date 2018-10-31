c1e8ac21b1b6e0af20f727a4027aecbe40bc25c5
Kasper Hansen <kaha.skp@edu.mercantec.dk>
2018-10-31 08:49:02 +0100

    This should work
    Filsti og ændringer i program.cs

--- C#/Gruppe2Project/Gruppe2Project/Program.cs
+++ C#/Gruppe2Project/Gruppe2Project/Program.cs
@@ -10,41 +10,33 @@ namespace Gruppe2Project
         static void Main(string[] args)
         {
             Program p = new Program();
-<<<<<<< HEAD
+
             Console.WriteLine("Type in the name of the file you wanna use.");
             p.Start();            
-=======
 
-            p.Start();
-
->>>>>>> Filstitest
         }
 
         public void Start()
         {
             Functions f = new Functions();
-<<<<<<< HEAD
-            string Choice = Console.ReadLine();            
-=======
+
 
             Console.WriteLine(f.GetCurrentPath());
             //gets user input
             string Choice = Console.ReadLine();
->>>>>>> Filstitest
 
             //if the user input is the same as "MyText" it wil get the current path of the file
             if (Choice == "MyText") // Choose File (if multiple)
             {
-<<<<<<< HEAD
+
                 Console.Clear();
                 Console.WriteLine("The file 'MyText' have been choosen.");
                 Thread.Sleep(2000);
                 Console.Clear();
 
-                path = @"/..MyText.txt";
-=======
+
                 path = f.GetCurrentPath();
->>>>>>> Filstitest
+
                 f.FileCall(path);
             }
         }
